e6b9ce73cd95f693796801526585f5857ad377ae
Imna <gio.imnaishvili029@gmail.com>
2025-01-27 13:57:41 +0400

    Add explicit types to formatter functions

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -3,14 +3,14 @@ import { format } from "date-fns";
 /*
  * Formats a date object into a string
  */
-export const formatFlightDate = (date?: Date) => {
+export const formatFlightDate = (date?: Date): string | null => {
   return date ? format(date, "yyyy-MM-dd") : null;
 };
 
 /*
  * Formats a date object into a string for My Trips display
  */
-export const formatDisplayDate = (date: Date) =>
+export const formatDisplayDate = (date: Date): string =>
   new Intl.DateTimeFormat("en", {
     month: "short",
     day: "numeric",
